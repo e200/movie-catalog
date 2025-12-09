fda554a65833de685db6fa7a1a72d014c67fcafa
e200 <eleandro@inbox.ru>
2025-12-09 14:22:02 +0000

    guhyoluy

--- lib/movie/controllers/favourite_movies.dart
+++ lib/movie/controllers/favourite_movies.dart
@@ -2,5 +2,5 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:movie_catalog/movie/model/movie.dart';
 
 final favouriteMoviesProvider = StateProvider((ref) {
-  return <Movie>[ ];
+  return <Movie>[];
 });
